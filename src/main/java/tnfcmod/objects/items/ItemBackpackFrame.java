b9b931dc423b7817646ad7f3a2e30705643ca284
AnodeCathode <shotorius_dt@hotmail.com>
2020-05-31 13:36:19 -0600

    Add font. Don't know if this works. Stolen from 1.7.10
    Oh well.

--- src/main/java/tnfcmod/objects/items/ItemBackpackFrame.java
+++ src/main/java/tnfcmod/objects/items/ItemBackpackFrame.java
@@ -1,13 +1,18 @@
 package tnfcmod.objects.items;
 
-import net.minecraft.creativetab.CreativeTabs;
-import net.minecraft.item.Item;
+import javax.annotation.Nonnull;
 
+import net.minecraft.creativetab.CreativeTabs;
+import net.minecraft.item.ItemStack;
+
+import net.dries007.tfc.api.capability.size.Size;
+import net.dries007.tfc.api.capability.size.Weight;
+import net.dries007.tfc.objects.items.ItemTFC;
 import tnfcmod.tnfcmod;
 
 import static tnfcmod.tnfcmod.MODID;
 
-public class ItemBackpackFrame extends Item
+public class ItemBackpackFrame extends ItemTFC
 {
     protected String name;
     public ItemBackpackFrame(String name)
@@ -26,5 +31,19 @@ public class ItemBackpackFrame extends Item
     public ItemBackpackFrame setCreativeTab(CreativeTabs tab) {
         super.setCreativeTab(tab);
         return this;
+    }
+
+    @Nonnull
+    @Override
+    public Size getSize(@Nonnull ItemStack itemStack)
+    {
+        return Size.NORMAL;
+    }
+
+    @Nonnull
+    @Override
+    public Weight getWeight(@Nonnull ItemStack itemStack)
+    {
+        return Weight.MEDIUM;
     }
 }
