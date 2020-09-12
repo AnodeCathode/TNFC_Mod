f578813b1d5c36e48665ab83dbc068cbce4de1e7
AnodeCathode <shotorius_dt@hotmail.com>
2020-09-12 15:26:06 -0600

    Add straw and ice blocks as heating/cooling items
    Amount in a stack doesn't matter. Each slot is counted as 1

--- src/main/java/com/lumintorious/ambiental/modifiers/ItemModifier.java
+++ src/main/java/com/lumintorious/ambiental/modifiers/ItemModifier.java
@@ -1,22 +1,19 @@
 package com.lumintorious.ambiental.modifiers;
 
-import java.util.HashMap;
-import java.util.Map;
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.init.Blocks;
+import net.minecraft.init.Items;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
 
-import com.lumintorious.ambiental.TFCAmbientalConfig;
 import com.lumintorious.ambiental.api.IItemTemperatureOwner;
 import com.lumintorious.ambiental.api.IItemTemperatureProvider;
 import com.lumintorious.ambiental.api.TemperatureRegistry;
-
 import net.dries007.tfc.api.capability.heat.CapabilityItemHeat;
-import net.dries007.tfc.api.capability.heat.Heat;
 import net.dries007.tfc.api.capability.heat.IItemHeat;
-import net.dries007.tfc.api.recipes.heat.HeatRecipe;
-import net.dries007.tfc.compat.jei.categories.HeatCategory;
-import net.dries007.tfc.objects.items.ItemTFC;
-import net.minecraft.entity.player.EntityPlayer;
-import net.minecraft.item.ItemStack;
-import net.minecraft.util.EnumFacing;
+import net.dries007.tfc.objects.blocks.BlockIceTFC;
+import net.dries007.tfc.objects.blocks.BlocksTFC;
+import net.dries007.tfc.objects.items.ItemsTFC;
 
 public class ItemModifier extends BaseModifier{
 	
@@ -29,6 +26,8 @@ public class ItemModifier extends BaseModifier{
 	}
 	
 	public static void computeModifiers(EntityPlayer player, ModifierStorage modifiers) {
+	    int strawCount = 0;
+	    int iceCount = 0;
 		for(ItemStack stack : player.inventoryContainer.inventoryItemStacks) {
 			if(stack.hasCapability(CapabilityItemHeat.ITEM_HEAT_CAPABILITY, null)) {
 				IItemHeat heat = stack.getCapability(CapabilityItemHeat.ITEM_HEAT_CAPABILITY, null);
@@ -44,7 +43,22 @@ public class ItemModifier extends BaseModifier{
 			for(IItemTemperatureProvider provider : TemperatureRegistry.ITEMS) {
 				modifiers.add(provider.getModifier(stack, player));
 			}
+            //individual stacks of straw provide a small amount of cold protection. Stack size doesn't matter.
+			if (stack.getItem() == ItemsTFC.STRAW){
+                strawCount += 1;
+            }
+            if (stack.getItem() == Item.getItemFromBlock(BlocksTFC.SEA_ICE) || stack.getItem() == Item.getItemFromBlock(Blocks.ICE)){
+                iceCount += 1;
+            }
 		}
+        if (strawCount > 0 ){
+            modifiers.add(new ItemModifier("straw_insulation", strawCount * 0.4f, 0.2f));
+        }
+        if (iceCount > 0){
+            modifiers.add(new ItemModifier("iceblocks", iceCount* -0.4f, 0.2f));
+        }
+
 	}
+
 
 }
