78764a72bfec2e0b1f465f23be660a64c2e2fa15
AnodeCathode <shotorius_dt@hotmail.com>
2020-09-14 09:30:57 -0600

    Update BwMRecipes.java
    Reduce the log>plank output somewhat

--- src/main/java/tnfcmod/recipes/BwMRecipes.java
+++ src/main/java/tnfcmod/recipes/BwMRecipes.java
@@ -26,11 +26,11 @@ public class BwMRecipes
         for (BlockLogTFC block : BlocksTFC.getAllLogBlocks())
         {
             ItemStack log = new ItemStack(block, 1, 4);
-            BWRegistry.WOOD_SAW.addRecipe(log, Lists.newArrayList(new ItemStack(ItemLumberTFC.get(block.getWood()), 16), ItemMaterial.getMaterial(ItemMaterial.EnumMaterial.SAWDUST, 4)));
+            BWRegistry.WOOD_SAW.addRecipe(log, Lists.newArrayList(new ItemStack(ItemLumberTFC.get(block.getWood()), 12), ItemMaterial.getMaterial(ItemMaterial.EnumMaterial.SAWDUST, 4)));
             ItemStack log2 = new ItemStack(block, 1, 5);
-            BWRegistry.WOOD_SAW.addRecipe(log2, Lists.newArrayList(new ItemStack(ItemLumberTFC.get(block.getWood()), 16), ItemMaterial.getMaterial(ItemMaterial.EnumMaterial.SAWDUST, 4)));
+            BWRegistry.WOOD_SAW.addRecipe(log2, Lists.newArrayList(new ItemStack(ItemLumberTFC.get(block.getWood()), 12), ItemMaterial.getMaterial(ItemMaterial.EnumMaterial.SAWDUST, 4)));
             ItemStack log3 = new ItemStack(block, 1, 6);
-            BWRegistry.WOOD_SAW.addRecipe(log3, Lists.newArrayList(new ItemStack(ItemLumberTFC.get(block.getWood()), 16), ItemMaterial.getMaterial(ItemMaterial.EnumMaterial.SAWDUST, 4)));
+            BWRegistry.WOOD_SAW.addRecipe(log3, Lists.newArrayList(new ItemStack(ItemLumberTFC.get(block.getWood()), 12), ItemMaterial.getMaterial(ItemMaterial.EnumMaterial.SAWDUST, 4)));
         }
 
     }
