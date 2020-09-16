603cc2b4b136cb807f0a8505ddd0cd8c861078f0
AnodeCathode <shotorius_dt@hotmail.com>
2020-09-16 02:44:06 -0600

    Update LeatherTunicBauble.java
    Slowness effect while worn.

--- src/main/java/tnfcmod/objects/items/LeatherTunicBauble.java
+++ src/main/java/tnfcmod/objects/items/LeatherTunicBauble.java
@@ -3,15 +3,21 @@ package tnfcmod.objects.items;
 import javax.annotation.Nonnull;
 
 import net.minecraft.entity.EntityLivingBase;
+import net.minecraft.init.MobEffects;
 import net.minecraft.inventory.EntityEquipmentSlot;
 import net.minecraft.item.ItemArmor;
 import net.minecraft.item.ItemStack;
+import net.minecraft.potion.Potion;
+import net.minecraft.potion.PotionEffect;
+
+import net.minecraftforge.fml.common.registry.ForgeRegistries;
 
 import baubles.api.BaubleType;
 import baubles.api.IBauble;
 import net.dries007.tfc.api.capability.size.Size;
 import net.dries007.tfc.api.capability.size.Weight;
 import net.dries007.tfc.objects.items.ItemTFC;
+import net.dries007.tfc.objects.potioneffects.PotionEffectsTFC;
 import tnfcmod.tnfcmod;
 
 import static tnfcmod.tnfcmod.MODID;
@@ -40,7 +46,7 @@ public class LeatherTunicBauble extends ItemTFC implements IBauble
     @Override
     public void onWornTick(ItemStack itemstack, EntityLivingBase player)
     {
-
+        player.addPotionEffect(new PotionEffect(MobEffects.SLOWNESS, 5, 125, false, false));
     }
 
     @Override
