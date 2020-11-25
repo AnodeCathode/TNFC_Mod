6e6564c863593d77c8de5e9807f5f6f81c837853
AnodeCathode <shotorius_dt@hotmail.com>
2020-11-25 14:51:54 -0700

    Maintenance and updates of libs

---
+++ RecipeScript/GenerateStorageDrawerRecipes.py
@@ -0,0 +1,28 @@
+WOOD_TYPES = {
+    'acacia': 'acacia',
+    'ash': 'normal',
+    'aspen': 'normal',
+    'birch': 'normal',
+    'blackwood': 'tall',
+    'chestnut': 'normal',
+    'douglas_fir': 'tallXL',
+    'hickory': 'normal',
+    'maple': 'normal',
+    'oak': 'tallXL',
+    'palm': 'tropical',
+    'pine': 'conifer',
+    'rosewood': 'tall',
+    'sequoia': 'sequoia',
+    'spruce': 'conifer',
+    'sycamore': 'normal',
+    'white_cedar': 'tall',
+    'willow': 'willow',
+    'kapok': 'jungle'
+}
+
+DRAWER_TYPES = {
+    'extra_drawers',
+    'extra_trim_0'
+    'extra_trim_1'
+
+}
