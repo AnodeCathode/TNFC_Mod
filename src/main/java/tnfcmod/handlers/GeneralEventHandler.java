9a3639c18d7324aba69ea33c67981cdf19f83537
AnodeCathode <shotorius_dt@hotmail.com>
2020-12-10 17:00:10 -0700

    Don't need this with the Borg method

--- src/main/java/tnfcmod/handlers/GeneralEventHandler.java
+++ src/main/java/tnfcmod/handlers/GeneralEventHandler.java
@@ -33,6 +33,7 @@ import net.minecraft.world.World;
 import net.minecraftforge.event.entity.EntityJoinWorldEvent;
 import net.minecraftforge.event.entity.living.LivingDeathEvent;
 import net.minecraftforge.event.entity.living.LivingEntityUseItemEvent;
+import net.minecraftforge.event.entity.living.LivingEvent;
 import net.minecraftforge.event.entity.player.PlayerEvent;
 import net.minecraftforge.event.world.BlockEvent;
 import net.minecraftforge.fml.common.Mod;
@@ -51,6 +52,7 @@ import ichttt.mods.firstaid.api.damagesystem.AbstractPlayerDamageModel;
 import net.dries007.tfc.ConfigTFC;
 import net.dries007.tfc.Constants;
 import net.dries007.tfc.api.capability.food.*;
+import net.dries007.tfc.api.capability.player.CapabilityPlayerData;
 import net.dries007.tfc.api.registries.TFCRegistries;
 import net.dries007.tfc.api.types.IPredator;
 import net.dries007.tfc.api.types.Tree;
@@ -62,6 +64,9 @@ import net.dries007.tfc.objects.blocks.wood.BlockLeavesTFC;
 import net.dries007.tfc.objects.blocks.wood.BlockSaplingTFC;
 import net.dries007.tfc.types.DefaultPlants;
 import net.dries007.tfc.util.OreDictionaryHelper;
+import net.dries007.tfc.util.config.OreTooltipMode;
+import net.dries007.tfc.util.skills.SkillTier;
+import net.dries007.tfc.util.skills.SkillType;
 import net.dries007.tfc.world.classic.chunkdata.ChunkDataTFC;
 import tnfcmod.util.ConfigTNFCMod;
 import tnfcmod.util.MonsterGear;
@@ -156,81 +161,46 @@ public class GeneralEventHandler
             //And makes you thirsty
             newFoodStats.addThirst(-20);
 
-//            float healthModifier = ((IFoodStatsTFC) player.getFoodStats()).getHealthModifier();
-//            if (healthModifier < ConfigTFC.General.PLAYER.minHealthModifier)
-//            {
-//                healthModifier = (float) ConfigTFC.General.PLAYER.minHealthModifier;
-//            }
-//            if (healthModifier > ConfigTFC.General.PLAYER.maxHealthModifier)
-//            {
-//                healthModifier = (float) ConfigTFC.General.PLAYER.maxHealthModifier;
-//            }
-//
-//            healthModifier = healthModifier + 0.15f; //Add the fudge factor to make the starting healthModifier 1, this simplifies a whole bunch of BS.
-//
-//            float curHealth = player.getHealth();
-//            float basePercentage = curHealth / 20;
-//
-//
-//            AbstractPlayerDamageModel damageModel = Objects.requireNonNull(player.getCapability(CapabilityExtendedHealthSystem.INSTANCE, null));
-//            for (AbstractDamageablePart damageablePart : damageModel)
-//            {
-//                float partHealth = damageablePart.currentHealth;
-//                float partMax = damageablePart.getMaxHealth();
-//                float partPercentage = partHealth / partMax;
-//
-//                if (basePercentage == 1)
-//                {
-//                    partPercentage = 1;
-//                }
-//                int initialMax = damageablePart.initialMaxHealth;
-//                float newMax = initialMax * healthModifier;
-//                int newInt = (int) Math.ceil(newMax);
-//
-//                damageablePart.setMaxHealth(newInt);
-//                damageablePart.currentHealth = Math.min(newInt * partPercentage, newInt);
-//
-//            }
         }
 
     }
 
 
-//    @SubscribeEvent
-//    public static void onPlayerUpdate(LivingUpdateEvent event)
-//    {
-//        if (!(event.getEntityLiving() instanceof EntityPlayer))
-//        {
-//            return;
-//        }
-//
-//        EntityPlayer player = (EntityPlayer) event.getEntityLiving();
-//        if (player.isCreative() || player.isSpectator())
-//        {
-//            return;
-//        }
-//
-//
-//        if (ConfigTNFCMod.GENERAL.skillbasedTempDisplay)
-//        {
-//            SkillTier tier = CapabilityPlayerData.getSkill(player, SkillType.SMITHING).getTier();
-//            if (tier.isAtLeast(SkillTier.EXPERT))
-//            {
-//
-//                if (ConfigTFC.Client.TOOLTIP.oreTooltipMode != OreTooltipMode.ADVANCED)
-//                {
-//                    ConfigTFC.Client.TOOLTIP.oreTooltipMode = OreTooltipMode.ADVANCED;
-//                }
-//            }
-//            else
-//            {
-//                if (ConfigTFC.Client.TOOLTIP.oreTooltipMode == OreTooltipMode.ADVANCED)
-//                {
-//                    ConfigTFC.Client.TOOLTIP.oreTooltipMode = OreTooltipMode.ALL_INFO;
-//                }
-//            }
-//        }
-//    }
+    @SubscribeEvent
+    public static void onPlayerUpdate(LivingEvent.LivingUpdateEvent event)
+    {
+        if (!(event.getEntityLiving() instanceof EntityPlayer))
+        {
+            return;
+        }
+
+        EntityPlayer player = (EntityPlayer) event.getEntityLiving();
+        if (player.isCreative() || player.isSpectator())
+        {
+            return;
+        }
+
+
+        if (ConfigTNFCMod.GENERAL.skillbasedTempDisplay)
+        {
+            SkillTier tier = CapabilityPlayerData.getSkill(player, SkillType.SMITHING).getTier();
+            if (tier.isAtLeast(SkillTier.EXPERT))
+            {
+
+                if (ConfigTFC.Client.TOOLTIP.oreTooltipMode != OreTooltipMode.ADVANCED)
+                {
+                    ConfigTFC.Client.TOOLTIP.oreTooltipMode = OreTooltipMode.ADVANCED;
+                }
+            }
+            else
+            {
+                if (ConfigTFC.Client.TOOLTIP.oreTooltipMode == OreTooltipMode.ADVANCED)
+                {
+                    ConfigTFC.Client.TOOLTIP.oreTooltipMode = OreTooltipMode.ALL_INFO;
+                }
+            }
+        }
+    }
 
 
     @SubscribeEvent
@@ -399,53 +369,6 @@ public class GeneralEventHandler
 
 
     }
-
-
-//    @SubscribeEvent
-//    public static void onPlayerLoggedIn(net.minecraftforge.fml.common.gameevent.PlayerEvent.PlayerLoggedInEvent event)
-//    {
-//        if (event.player instanceof EntityPlayerMP && ServerUtils.isSurvivalOrAdventure(event.player))
-//        {
-//            if (event.player.getFoodStats() instanceof IFoodStatsTFC)
-//            {
-//                float healthModifier = ((IFoodStatsTFC) event.player.getFoodStats()).getHealthModifier();
-//                if (healthModifier < ConfigTFC.General.PLAYER.minHealthModifier)
-//                {
-//                    healthModifier = (float) ConfigTFC.General.PLAYER.minHealthModifier;
-//                }
-//                if (healthModifier > ConfigTFC.General.PLAYER.maxHealthModifier)
-//                {
-//                    healthModifier = (float) ConfigTFC.General.PLAYER.maxHealthModifier;
-//                }
-//
-//                healthModifier = healthModifier + 0.15f; //Add the fudge factor to make the starting healthModifier 1, this simplifies a whole bunch of BS.
-//
-//                float curHealth = event.player.getHealth();
-//                float basePercentage = curHealth / 20;
-//
-//
-//                AbstractPlayerDamageModel damageModel = Objects.requireNonNull(event.player.getCapability(CapabilityExtendedHealthSystem.INSTANCE, null));
-//                for (AbstractDamageablePart damageablePart : damageModel)
-//                {
-//                    float partHealth = damageablePart.currentHealth;
-//                    float partMax = damageablePart.getMaxHealth();
-//                    float partPercentage = partHealth / partMax;
-//
-//                    if (basePercentage == 1)
-//                    {
-//                        partPercentage = 1;
-//                    }
-//                    int initialMax = damageablePart.initialMaxHealth;
-//                    float newMax = initialMax * healthModifier;
-//                    int newInt = (int) Math.ceil(newMax);
-//
-//                    damageablePart.setMaxHealth(newInt);
-//                    damageablePart.currentHealth = Math.min(newInt * partPercentage, newInt);
-//
-//                }
-//            }
-//        }
-//    }
 
 
     @SubscribeEvent
