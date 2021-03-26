d22700b02ba2ea6d3f20534521f5d71f43036f6c
AnodeCathode <shotorius_dt@hotmail.com>
2021-03-26 11:03:20 -0600

    Instantiate aged drinks properly

--- src/main/java/com/aged_drinks/TFCAgedDrinks.java
+++ src/main/java/com/aged_drinks/TFCAgedDrinks.java
@@ -1,12 +1,13 @@
 package com.aged_drinks;
 
-import net.dries007.tfc.api.recipes.barrel.BarrelRecipe;
-
 import net.minecraftforge.event.RegistryEvent;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.Mod.EventHandler;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
+
+import net.dries007.tfc.api.recipes.barrel.BarrelRecipe;
+
 
 @Mod(modid = TFCAgedDrinks.MODID, name = TFCAgedDrinks.NAME, version = TFCAgedDrinks.VERSION)
 public class TFCAgedDrinks
@@ -15,17 +16,21 @@ public class TFCAgedDrinks
     public static final String NAME = "TNFC Aged Drinks";
     public static final String VERSION = "12.2.65";
 
+    @Mod.Instance(MODID)
+    public static TFCAgedDrinks INSTANCE;
+
     @EventHandler
     public void init(FMLInitializationEvent event)
     {
     	
     }
-    
+
     @Mod.EventBusSubscriber
     public static class ObjectRegistryHandler {
-    	@SubscribeEvent
+
+        @SubscribeEvent
         public static void onRegisterBarrelRecipeEvent(RegistryEvent.Register<BarrelRecipe> event) {
-    		AgedRegistry.registerAgedDrinks(event);
+            AgedRegistry.registerAgedDrinks(event);
         }
     }
     
