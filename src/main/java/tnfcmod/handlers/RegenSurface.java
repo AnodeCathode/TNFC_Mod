d5ff752a40b86b0011e0c60769ef70a351295cf2
AnodeCathode <shotorius_dt@hotmail.com>
2021-08-12 08:14:19 -0600

    Disable TNFCMod chunk regen
    Now part of TFC

--- src/main/java/tnfcmod/handlers/RegenSurface.java
+++ src/main/java/tnfcmod/handlers/RegenSurface.java
@@ -56,111 +56,111 @@ import static tnfcmod.tnfcmod.MODID;
 public class RegenSurface
 {
 
-    private static final RegenRocksSticks ROCKS_GEN = new RegenRocksSticks(true);
-    private static final RegenWildCrops CROPS_GEN = new RegenWildCrops();
-    private static final WorldGenBerryBushes BUSH_GEN = new WorldGenBerryBushes();
-    public static final WorldGenPlantTFC PLANT_GEN = new WorldGenPlantTFC();
-    private static final Random RANDOM = new Random();
-    private static final List<ChunkPos> POSITIONS = new LinkedList<>();
+//    private static final RegenRocksSticks ROCKS_GEN = new RegenRocksSticks(true);
+//    private static final RegenWildCrops CROPS_GEN = new RegenWildCrops();
+//    private static final WorldGenBerryBushes BUSH_GEN = new WorldGenBerryBushes();
+//    public static final WorldGenPlantTFC PLANT_GEN = new WorldGenPlantTFC();
+//    private static final Random RANDOM = new Random();
+//    private static final List<ChunkPos> POSITIONS = new LinkedList<>();
 
 
-    @SubscribeEvent
-    public static void onChunkLoad(ChunkDataEvent.Load event)
-    {
+//    @SubscribeEvent
+//    public static void onChunkLoad(ChunkDataEvent.Load event)
+//    {
+//
+//        ChunkDataTFC chunkDataTFC = ChunkDataTFC.get(event.getChunk());
+//
+//
+//        //stick/rock/crop/mushroom/other? regen
+//        if (event.getWorld().provider.getDimension() == 0 &&  chunkDataTFC.isInitialized() && POSITIONS.size() < 1000)
+//        {
+//            //Only run this in the early months of each year
+//            if (CalendarTFC.CALENDAR_TIME.getMonthOfYear().isWithin(Month.APRIL, Month.JULY) && !chunkDataTFC.isSpawnProtected() && CalendarTFC.CALENDAR_TIME.getTotalYears() > chunkDataTFC.getLastUpdateYear())
+//            {
+//                POSITIONS.add(event.getChunk().getPos());
+//            }
+//        }
+//    }
 
-        ChunkDataTFC chunkDataTFC = ChunkDataTFC.get(event.getChunk());
-
-
-        //stick/rock/crop/mushroom/other? regen
-        if (event.getWorld().provider.getDimension() == 0 &&  chunkDataTFC.isInitialized() && POSITIONS.size() < 1000)
-        {
-            //Only run this in the early months of each year
-            if (CalendarTFC.CALENDAR_TIME.getMonthOfYear().isWithin(Month.APRIL, Month.JULY) && !chunkDataTFC.isSpawnProtected() && CalendarTFC.CALENDAR_TIME.getTotalYears() > chunkDataTFC.getLastUpdateYear())
-            {
-                POSITIONS.add(event.getChunk().getPos());
-            }
-        }
-    }
-
-    @SubscribeEvent
-    public static void onWorldTick(TickEvent.WorldTickEvent event)
-    {
-        if (!event.world.isRemote && event.phase == TickEvent.Phase.END)
-        {
-            if (!POSITIONS.isEmpty())
-            {
-                ServerUtils su = new ServerUtils();
-                double tps = su.getTPS(event.world, 0);
-                if (tps > 16)
-                {
-                    ChunkPos pos = POSITIONS.remove(0);
-                    Chunk chunk = event.world.getChunk(pos.x, pos.z);
-                    BlockPos blockPos = pos.getBlock(0, 0, 0);
-                    ChunkDataTFC chunkDataTFC = ChunkDataTFC.get(event.world, pos.getBlock(0, 0, 0));
-                    IChunkProvider chunkProvider = event.world.getChunkProvider();
-                    IChunkGenerator chunkGenerator = ((ChunkProviderServer) chunkProvider).chunkGenerator;
-
-
-                    if (CalendarTFC.CALENDAR_TIME.getMonthOfYear().isWithin(Month.APRIL, Month.JULY) && !chunkDataTFC.isSpawnProtected() && CalendarTFC.CALENDAR_TIME.getTotalYears() > chunkDataTFC.getLastUpdateYear())
-                    {
-                        if (ConfigTFC.General.WORLD_REGEN.sticksRocksModifier > 0)
-                        {
-                            //Nuke any rocks and sticks in chunk.
-                            removeAllPlacedItems(event.world, pos);
-                            List<Tree> trees = chunkDataTFC.getValidTrees();
-                            double rockModifier = ConfigTFC.General.WORLD_REGEN.sticksRocksModifier;
-                            ROCKS_GEN.generate(RANDOM, pos.x, pos.z, event.world, chunkGenerator, chunkProvider);
-
-                            final float density = chunkDataTFC.getFloraDensity();
-                            int stickDensity = 3 + (int) (4f * density + 1.5f * trees.size() * rockModifier);
-                            if (trees.isEmpty())
-                            {
-                                stickDensity = 1 + (int) (1.5f * density * rockModifier);
-                            }
-                            RegenRocksSticks.generateLooseSticks(RANDOM, pos.x, pos.z, event.world, stickDensity);
-                        }
-
-                        //Nuke crops/mushrooms/dead crops (not sure the latter is working.
-                        removeAllSurfaceCrap(event.world, pos);
-                        removeSeedbags(event.world, pos);
-
-                        float avgTemperature = ClimateTFC.getAvgTemp(event.world, blockPos);
-                        float rainfall = ChunkDataTFC.getRainfall(event.world, blockPos);
-                        float floraDensity = chunkDataTFC.getFloraDensity(); // Use for various plant based decoration (tall grass, those vanilla jungle shrub things, etc.)
-                        float floraDiversity = chunkDataTFC.getFloraDiversity();
-                        Plant plant = TFCRegistries.PLANTS.getValue(DefaultPlants.PORCINI);
-                        PLANT_GEN.setGeneratedPlant(plant);
-                        int mushroomCount = 3;
-                        for (float i = RANDOM.nextInt(Math.round(mushroomCount / floraDiversity)); i < (1 + floraDensity) * 5; i++)
-                        {
-                            BlockPos blockMushroomPos = event.world.getHeight(blockPos.add(RANDOM.nextInt(16) + 8, 0, RANDOM.nextInt(16) + 8));
-                            PLANT_GEN.generate(event.world, RANDOM, blockMushroomPos);
-                        }
-                        CROPS_GEN.generate(RANDOM, pos.x, pos.z, event.world, chunkGenerator, chunkProvider);
-                        int worldX = pos.x << 4;
-                        int worldZ = pos.z << 4;
-                        BlockPos blockpos = new BlockPos(worldX, 0, worldZ);
-                        Biome biome = event.world.getBiome(blockpos.add(16, 0, 16));
-                        regenPredators(event.world, biome, worldX + 8, worldZ + 8, 16, 16, RANDOM);
-
-                        //Should nuke any bushes in the chunk. For now we just leave the bushes alone.
-                        //BUSH_GEN.generate(RANDOM, pos.x, pos.z, event.world, chunkGenerator, chunkProvider);
-                        chunkDataTFC.resetLastUpdateYear();
-
-
-                    }
-                    chunk.markDirty();
-                    ((ChunkProviderServer) chunkProvider).queueUnload(chunk);
-
-                }
-                else //TPS too low. Just remove it and move on. Or do we leave it?
-                {
-                    ChunkPos pos = POSITIONS.remove(0);
-                }
-            }
-        }
-
-    }
+//    @SubscribeEvent
+//    public static void onWorldTick(TickEvent.WorldTickEvent event)
+//    {
+//        if (!event.world.isRemote && event.phase == TickEvent.Phase.END)
+//        {
+//            if (!POSITIONS.isEmpty())
+//            {
+//                ServerUtils su = new ServerUtils();
+//                double tps = su.getTPS(event.world, 0);
+//                if (tps > 16)
+//                {
+//                    ChunkPos pos = POSITIONS.remove(0);
+//                    Chunk chunk = event.world.getChunk(pos.x, pos.z);
+//                    BlockPos blockPos = pos.getBlock(0, 0, 0);
+//                    ChunkDataTFC chunkDataTFC = ChunkDataTFC.get(event.world, pos.getBlock(0, 0, 0));
+//                    IChunkProvider chunkProvider = event.world.getChunkProvider();
+//                    IChunkGenerator chunkGenerator = ((ChunkProviderServer) chunkProvider).chunkGenerator;
+//
+//
+//                    if (CalendarTFC.CALENDAR_TIME.getMonthOfYear().isWithin(Month.APRIL, Month.JULY) && !chunkDataTFC.isSpawnProtected() && CalendarTFC.CALENDAR_TIME.getTotalYears() > chunkDataTFC.getLastUpdateYear())
+//                    {
+//                        if (ConfigTFC.General.WORLD_REGEN.sticksRocksModifier > 0)
+//                        {
+//                            //Nuke any rocks and sticks in chunk.
+//                            removeAllPlacedItems(event.world, pos);
+//                            List<Tree> trees = chunkDataTFC.getValidTrees();
+//                            double rockModifier = ConfigTFC.General.WORLD_REGEN.sticksRocksModifier;
+//                            ROCKS_GEN.generate(RANDOM, pos.x, pos.z, event.world, chunkGenerator, chunkProvider);
+//
+//                            final float density = chunkDataTFC.getFloraDensity();
+//                            int stickDensity = 3 + (int) (4f * density + 1.5f * trees.size() * rockModifier);
+//                            if (trees.isEmpty())
+//                            {
+//                                stickDensity = 1 + (int) (1.5f * density * rockModifier);
+//                            }
+//                            RegenRocksSticks.generateLooseSticks(RANDOM, pos.x, pos.z, event.world, stickDensity);
+//                        }
+//
+//                        //Nuke crops/mushrooms/dead crops (not sure the latter is working.
+//                        removeAllSurfaceCrap(event.world, pos);
+//                        removeSeedbags(event.world, pos);
+//
+//                        float avgTemperature = ClimateTFC.getAvgTemp(event.world, blockPos);
+//                        float rainfall = ChunkDataTFC.getRainfall(event.world, blockPos);
+//                        float floraDensity = chunkDataTFC.getFloraDensity(); // Use for various plant based decoration (tall grass, those vanilla jungle shrub things, etc.)
+//                        float floraDiversity = chunkDataTFC.getFloraDiversity();
+//                        Plant plant = TFCRegistries.PLANTS.getValue(DefaultPlants.PORCINI);
+//                        PLANT_GEN.setGeneratedPlant(plant);
+//                        int mushroomCount = 3;
+//                        for (float i = RANDOM.nextInt(Math.round(mushroomCount / floraDiversity)); i < (1 + floraDensity) * 5; i++)
+//                        {
+//                            BlockPos blockMushroomPos = event.world.getHeight(blockPos.add(RANDOM.nextInt(16) + 8, 0, RANDOM.nextInt(16) + 8));
+//                            PLANT_GEN.generate(event.world, RANDOM, blockMushroomPos);
+//                        }
+//                        CROPS_GEN.generate(RANDOM, pos.x, pos.z, event.world, chunkGenerator, chunkProvider);
+//                        int worldX = pos.x << 4;
+//                        int worldZ = pos.z << 4;
+//                        BlockPos blockpos = new BlockPos(worldX, 0, worldZ);
+//                        Biome biome = event.world.getBiome(blockpos.add(16, 0, 16));
+//                        regenPredators(event.world, biome, worldX + 8, worldZ + 8, 16, 16, RANDOM);
+//
+//                        //Should nuke any bushes in the chunk. For now we just leave the bushes alone.
+//                        //BUSH_GEN.generate(RANDOM, pos.x, pos.z, event.world, chunkGenerator, chunkProvider);
+//                        chunkDataTFC.resetLastUpdateYear();
+//
+//
+//                    }
+//                    chunk.markDirty();
+//                    ((ChunkProviderServer) chunkProvider).queueUnload(chunk);
+//
+//                }
+//                else //TPS too low. Just remove it and move on. Or do we leave it?
+//                {
+//                    ChunkPos pos = POSITIONS.remove(0);
+//                }
+//            }
+//        }
+//
+//    }
 
     private static void removeAllSurfaceCrap(World world, ChunkPos pos)
     {
