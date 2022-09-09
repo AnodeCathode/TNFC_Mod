ab57aafcadc7dd0bae6b2325dc2c571d3a67f261
Gaelmare <2gaelmare@gmail.com>
2022-09-09 00:45:14 -0600

    mixin try 3, thanks to a cast of characters!

--- src/main/java/tnfcmod/mixin/ArrowHandlerMixin.java
+++ src/main/java/tnfcmod/mixin/ArrowHandlerMixin.java
@@ -2,6 +2,7 @@ package tnfcmod.mixin;
 
 import net.dries007.tfc.objects.items.ItemQuiver;
 import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.item.ItemStack;
 import net.minecraft.util.ActionResult;
 import net.minecraft.util.EnumActionResult;
 import net.minecraftforge.event.entity.player.ArrowNockEvent;
@@ -15,9 +16,31 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 public class ArrowHandlerMixin {
 
     @Redirect(method = "Lcofh/core/proxy/EventHandler;handleArrowNockEvent(Lnet/minecraftforge/event/entity/player/ArrowNockEvent;)V",
-            at = @At(value="INVOKE", target = "setAction",
-                     ordinal = 1),
+            at = @At(value="INVOKE", target = "Lnet/minecraftforge/event/entity/player/ArrowNockEvent;setAction(Lnet/minecraft/util/ActionResult;)V",
+                    ordinal = 1),
             remap = false)
+    public void checkTFCQuiver(ActionResult<ItemStack> action, ArrowNockEvent event, CallbackInfo ci)
+    {
+        final EntityPlayer player = event.getEntityPlayer();
+        if (player != null && !player.capabilities.isCreativeMode && ItemQuiver.replenishArrow(player))
+        {
+            event.setAction(new ActionResult<>(EnumActionResult.SUCCESS, event.getBow()));
+        }
+        else
+        {
+            event.setAction(new ActionResult<>(EnumActionResult.FAIL, event.getBow()));
+        }
+    }
+}
+
+/*
+
+@Mixin(EventHandler.class)
+public class ArrowHandlerMixin {
+
+    @Redirect(method = "Lcofh/core/proxy/EventHandler;handleArrowNockEvent(Lnet/minecraftforge/event/entity/player/ArrowNockEvent;)V",
+            at = @At(value="INVOKE", target = "Lnet/minecraftforge/event/entity/player/ArrowNockEvent;setAction(Lnet/minecraft/util/ActionResult;)V",
+            ordinal = 1), remap = false)
     public void checkTFCQuiver(ArrowNockEvent event, CallbackInfo ci)
     {
         final EntityPlayer player = event.getEntityPlayer();
@@ -31,3 +54,4 @@ public class ArrowHandlerMixin {
         }
     }
 }
+*/
